faa72f7fabfe3b4a3360bb7439a343234480ead1
Kriti Rawani <kritisdl2003@gmail.com>
2026-05-28 23:58:09 +0530

    Fixed API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://breathe-esg-enne.onrender.com",
+  baseURL: "https://breathe-esg-enne.onrender.com/api",
 });
 
 export default API;
